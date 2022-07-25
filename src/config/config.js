d5f7b07567401c53d4eddeadc03f4a08e9592c91
Scott <1194379729@qq.com>
2022-07-25 21:21:23 +0800

    feat(config):新增mysql配置

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+/**
+ * 默认配置
+ * @author yupi
+ */
+module.exports = {
+  // MySQL 配置
+  dbConfig: {
+    database: 'scott-admin',
+    username: 'root',
+    password: 'root',
+    host: 'localhost',
+    port: 3306,
+  },
+};
