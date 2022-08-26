5c0fda10d5d2fcddb32aa0cdd55cf078dcdf190b
Scott <1194379729@qq.com>
2022-08-26 17:52:23 +0800

    feat: 新增文章类

---
+++ src/model/article.js
@@ -0,0 +1,57 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+/**
+ * 用户模型
+ * @author yupi
+ */
+const ArticleModel = sequelize.define(
+  "Article",
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Model attributes are defined here
+    articleType: {
+      type: DataTypes.STRING,
+      field: "article_type",
+    },
+    formUrl: {
+      type: DataTypes.STRING,
+      field: "form_url",
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    status: {
+      type: DataTypes.INTEGER,
+      //  1 启用 2 禁用
+      defaultValue: 1,
+    },
+    createTime: {
+      type: DataTypes.DATE,
+      field: "create_time",
+    },
+    updateTime: {
+      type: DataTypes.DATE,
+      field: "update_time",
+    },
+  },
+  {
+    tableName: "article",
+    // 软删除
+    paranoid: false,
+    createdAt: "createTime",
+    updatedAt: "updateTime",
+    // 自动添加时间戳
+    timestamps: true,
+  }
+);
+
+module.exports = ArticleModel;
